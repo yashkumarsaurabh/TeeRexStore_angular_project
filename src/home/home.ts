8d16354231cb553206c2c05d249588f19f1197b0
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2024-07-17 19:22:16 +0530

    created a home component for view

---
+++ src/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'home',
+    standalone: true,
+    template: ``,
+    styleUrls: ['./home.scss'],
+})
+export class Home { }
