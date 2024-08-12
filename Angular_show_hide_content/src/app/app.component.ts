4bb5cfbfc77c785da580a558acc427c0a4f9c9cf
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2024-08-12 17:17:00 +0530

    showHide function for a single item in list

--- Angular_show_hide_content/src/app/app.component.ts
+++ Angular_show_hide_content/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Item {
+  title: string,
+  description: string,
+  showDescription: boolean,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_show_hide_content';
+
+  items: Item[] = [
+    { title: 'Item 1', description: 'Description for item 1', showDescription: false },
+    { title: 'Item 2', description: 'Description for item 2', showDescription: false },
+  ];
+
+  showAllDescription() {
+    this.items.map((data)=>data.showDescription=true)
+  }
+
+  hideAllDescription() {
+    this.items.map((data)=>data.showDescription=false)
+  }
+
+  toggleDescription(item: Item) {
+    this.items.map((data) => data===item? data.showDescription=!data.showDescription: data);
+  }
 }
